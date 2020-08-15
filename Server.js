432dcb00586a4db76422f34de43c26f9c997ef8a
getirchallenger <getirchallenger@gmail.com>
2020-08-15 14:23:56 +0300

    init http server

---
+++ Server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+
+const PORT = 8080;
+
+module.exports = function () {
+
+  let http;
+  let app;
+
+  function main() {
+    initHttpServer();
+  }
+
+  function initHttpServer() {
+    app = express();
+    http = require("http").Server(app);
+
+    app.use(function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
+
+    registerEndPoints();
+ 
+    http.listen(PORT, function () {
+      console.log("Server is listening on *:" + PORT);
+    });
+  }
+
+  function registerEndPoints() {
+    app.post("/getData", handleGetData.bind(this));
+  }
+
+  function handleGetData(req, res) {
+    console.log("HandleGetData called");
+    res.send({ result: "OK" });
+  }
+
+  main();
+};
---
+++ main.js
@@ -0,0 +1,9 @@
+const Server = require("./Server");
+
+let serverInstance;
+
+function main() {
+    serverInstance = new Server();
+}
+
+main();
